5656b78dbaa7d89f9d5da942c949e27482751a32
unknown <emanuelbarile06@gmail.com>
2021-03-28 12:50:24 -0700

    2do commit

--- src/com/company/Empleado.java
+++ src/com/company/Empleado.java
@@ -5,4 +5,36 @@ public class Empleado {
     private string nombre;
     private string apellido;
     private float salario;
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public string getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(string nombre) {
+        this.nombre = nombre;
+    }
+
+    public string getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(string apellido) {
+        this.apellido = apellido;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
 }
